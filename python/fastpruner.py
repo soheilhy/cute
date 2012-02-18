3bc1949757427f28465cf59c2b82d162c702cdb2
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2012-02-18 00:38:33 -0500

    Adding minimum freq.

--- python/fastpruner.py
+++ python/fastpruner.py
@@ -11,9 +11,12 @@ def parse(line):
   return (line[0:first], float(line[first+1:second]), line[second+1:])
 
 if __name__ == '__main__':
+  min_freq = float(sys.argv[1])
   trie = cute.utils.Trie()
   for line in sys.stdin:
     protocol, frequency, term = parse(line[:-1])
+    if not term or frequency < min_freq:
+      continue
     def f(t, term):
       existing_freq = t._protocol_count.get(protocol)
       if existing_freq and frequency < existing_freq:
@@ -21,8 +24,7 @@ if __name__ == '__main__':
       t._protocol_count[protocol] = frequency
       t._print_protocol[protocol] = len(term) == 1
 
-    if term:
-      trie.add(term, f)
+    trie.add(term, f)
 
   trie.print_protocol_counts(use_print_flag=True)
 
